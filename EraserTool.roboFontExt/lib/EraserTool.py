b92e1dd9620e9b61b1b3b1efbc37fdddb5282c44
Benedikt Bramböck <info@arialcrime.com>
2018-07-30 15:55:04 +0200

    minor fixes

--- EraserTool.roboFontExt/lib/EraserTool.py
+++ EraserTool.roboFontExt/lib/EraserTool.py
@@ -16,28 +16,23 @@ class EraserTool (EditingTool):
         g = CurrentGlyph()
         self.convertToLine(g)
             
-    def convertToLine(self, glyph, pointType):
-        print point
+    def convertToLine(self, glyph):
         if glyph and glyph.selection != []:
             glyph.prepareUndo()
             for c_index in range(len(glyph.contours)):
-			    c = glyph.contours[c_index]
-			    for s_index in range(len(c.segments)):
-				    s = c.segments[s_index]
-				    if s.selected and s.type == "curve":
-				        s.type = "line"
-				        s.points[0].smooth = False
-				        s.smooth = False
-				        c.segments[s_index-1].points[-1].smooth = False
-				        
+                c = glyph.contours[c_index]
+                for s_index in range(len(c.segments)):
+                    s = c.segments[s_index]
+                    if s.selected and s.type == "curve":
+                        s.type = "line"
+                        s.points[0].smooth = False
+                        s.smooth = False
+                        c.segments[s_index-1].points[-1].smooth = False
+                        
             glyph.deselect()
             UpdateCurrentGlyphView()
             glyph.update()
     
-    def modifiersChanged(self):
-        if self.optionDown:
-            self.pointType = 'oncurve'
-
     def getToolbarTip(self):
         return "Eraser Tool"
         
